ba69f711a783426b1267b8ef65d7cc026543ab72
brijeshLimbasiya <brijeshlimbasiya217@gmail.com>
2025-04-17 10:27:28 +0530

    OOPS

---
+++ PROJECT-OOP/q1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include <string.h>
+
+class bankaccount{
+    public:
+    char Accountnumber[30];
+    char Accountholdername[30];
+    int Balance;
+
+    public :
+};
+
+int main (){
+
+    return 0;
+
+}
